07297c1231a70d21ce51f948d2738b2fb2a41848
fauzan_lubis <f.lubis@indoleads.com>
2022-03-13 15:52:59 +0800

    update aws

--- config/plugins.js
+++ config/plugins.js
@@ -2,8 +2,8 @@ module.exports = ({ env }) => ({
   upload: {
     provider: 'aws-s3',
     providerOptions: {
-      accessKeyId: 'AKIAQRQUM6HEJY6AUE2Y',
-      secretAccessKey: '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',
+      accessKeyId: env('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: env('AWS_ACCESS_SECRET'),
       region: 'ap-southeast-1',
       params: {
         Bucket: 'cofitbucket',
