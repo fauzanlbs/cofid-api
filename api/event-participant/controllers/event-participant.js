b70fc0bfb3a35199616994eeaf5f291c0163da7e
fauzan_lubis <f.lubis@indoleads.com>
2022-03-29 17:36:43 +0800

    update event

--- api/event-participant/controllers/event-participant.js
+++ api/event-participant/controllers/event-participant.js
@@ -1,21 +1,31 @@
-const { sanitizeEntity } = require('strapi-utils');
+// const { sanitizeEntity } = require('strapi-utils');
 
-module.exports = {
-  /**
-   * Retrieve records.
-   *
-   * @return {Array}
-   */
+// module.exports = {
+//   /**
+//    * Retrieve records.
+//    *
+//    * @return {Array}
+//    */
 
-  async find(ctx) {
-    let entities;
-    if (ctx.query._q) {
-      entities = await strapi.services.eventParticipant.search(ctx.query);
-    } else {
-      // let populate = ['event', 'event.users_permissions_user']
-      entities = await strapi.services.eventParticipant.find(ctx.query);
-    }
+//   async find(ctx) {
+//     let entities;
+//     if (ctx.query._q) {
+//       entities = await strapi.services.eventParticipant.search(ctx.query);
+//     } else {
+//       // let populate = ['event', 'event.users_permissions_user']
+//       entities = await strapi.services.eventParticipant.find(ctx.query);
+//     }
 
-    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.eventParticipant }));
-  },
-};
+//     return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.eventParticipant }));
+//   },
+// };
+
+
+'use strict';
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {};
