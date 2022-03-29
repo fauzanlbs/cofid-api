33ac9f18da6cefbd46c8addc784c0167d5aa615e
fauzan_lubis <f.lubis@indoleads.com>
2022-03-29 16:34:55 +0800

    add event participant

--- api/event-participant/controllers/event-participant.js
+++ api/event-participant/controllers/event-participant.js
@@ -1,8 +1,20 @@
-'use strict';
+const { sanitizeEntity } = require('strapi-utils');
 
-/**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+module.exports = {
+  /**
+   * Retrieve records.
+   *
+   * @return {Array}
+   */
 
-module.exports = {};
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.event-participant.search(ctx.query);
+    } else {
+      entities = await strapi.services.event-participant.find(ctx.query, ['event', 'event.users_permissions_user']);
+    }
+
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.event-participant }));
+  },
+};
