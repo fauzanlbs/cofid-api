f09997d0f5d67df50e37d9704760bc5a020e85c7
fauzan_lubis <f.lubis@indoleads.com>
2022-03-29 16:44:54 +0800

    update event participant

--- api/event-participant/controllers/event-participant.js
+++ api/event-participant/controllers/event-participant.js
@@ -10,11 +10,11 @@ module.exports = {
   async find(ctx) {
     let entities;
     if (ctx.query._q) {
-      entities = await strapi.services.event-participant.search(ctx.query);
+      entities = await strapi.services.event_participant.search(ctx.query);
     } else {
-      entities = await strapi.services.event-participant.find(ctx.query, ['event', 'event.users_permissions_user']);
+      entities = await strapi.services.event_participant.find(ctx.query, ['event', 'event.users_permissions_user']);
     }
 
-    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.event-participant }));
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.event_participant }));
   },
 };
