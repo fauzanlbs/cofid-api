1a753c3b0eab3d11ef8a473fadb5028818b18780
fauzan_lubis <f.lubis@indoleads.com>
2022-03-29 19:15:06 +0800

    fix event

--- api/event-participant/controllers/event-participant.js
+++ api/event-participant/controllers/event-participant.js
@@ -10,22 +10,22 @@ module.exports = {
   async find(ctx) {
     let entities;
     if (ctx.query._q) {
-      entities = await strapi.services.eventparticipant.search(ctx.query);
+      entities = await strapi.services['event-participant'].search(ctx.query);
     } else {
-      // let populate = ['event', 'event.users_permissions_user']
-      entities = await strapi.services.eventparticipant.find(ctx.query);
+      let populate = ['event', 'event.users_permissions_user']
+      entities = await strapi.services['event-participant'].find(ctx.query, populate);
     }
 
-    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.eventparticipant }));
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models['event-participant'] }));
   },
 };
 
 
-// 'use strict';
+'use strict';
 
-// /**
-//  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
-//  * to customize this controller
-//  */
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * to customize this controller
+ */
 
-// module.exports = {};
+module.exports = {};
