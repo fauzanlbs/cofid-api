4f1471cb24ffb23368f9a2e70fda8425874932b5
Fauzan <fauzanlubis23@gmail.com>
2022-07-17 20:04:54 +0800

    update risk

--- api/profile/controllers/profile.js
+++ api/profile/controllers/profile.js
@@ -5,26 +5,25 @@ module.exports = {
     async calculateRisk(ctx) {
         
         const id = ctx.params.id;
-        const q2 = strapi.query('result').findOne({ users_permissions_user: id, questioner: 2 });
-        const q3 = strapi.query('result').findOne({ users_permissions_user: id, questioner: 3 });
-        const q4 = strapi.query('result').findOne({ users_permissions_user: id, questioner: 4 });
-        const q5 = strapi.query('result').findOne({ users_permissions_user: id, questioner: 5 });
-        const profile = strapi.query('profile').findOne({ users_permissions_user: id });
-
+        const q2 = await strapi.query('result').findOne({ users_permissions_user: id, questioner: 2 });
+        const q3 = await strapi.query('result').findOne({ users_permissions_user: id, questioner: 3 });
+        const q4 = await strapi.query('result').findOne({ users_permissions_user: id, questioner: 4 });
+        const q5 = await strapi.query('result').findOne({ users_permissions_user: id, questioner: 5 });
+        const profile = await strapi.query('profile').findOne({ users_permissions_user: id });
+        
         let total = q2.score ? q2.score : 0 + q3.score ? q3.score : 0 + q4.score ? q4.score : 0 + q5.score ? q5.score : 0;
 
         //default color is green
         let color_risk = 'green';
 
-        //if non honest, total will * 1.2
+               //if non honest, total will * 1.2
         if (profile.non_honest) {
             total = total * 1.2;
         }
-
+        
         //if have red zone status, will got red
-        if (profile.red_zone == 1) {
-            color_risk = 'red';
-            return color_risk;
+        if (profile.red_zone == true) {
+            return 'red';
         }
 
         //range
@@ -37,7 +36,7 @@ module.exports = {
             color_risk = 'orange';
         } else if (profile.consequence > 15 && (profile.non_honest || profile.non_masker || profile.non_masker_social)
         ) {
-            color_risk = 'red';
+            return 'red';
         }
 
         if (profile.consequence >= 8 && profile.consequence <= 15 && total < 22) {
@@ -51,7 +50,7 @@ module.exports = {
             color_risk = 'orange';
         } else if (profile.consequence >= 8 && profile.consequence <= 15 && (profile.non_honest || profile.non_masker || profile.non_masker_social)
         ) {
-            color_risk = 'red';
+            return 'red';
         }
 
         if (profile.consequence >= 1 && profile.consequence <= 7 && total < 22) {
@@ -79,6 +78,6 @@ module.exports = {
         }
 
         return color_risk;
+        }
 
-    }
 };
