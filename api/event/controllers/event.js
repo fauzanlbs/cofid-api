99072590af3e6fe4f850103b818826e0f1ceb3bd
Fauzan <fauzanlubis23@gmail.com>
2022-10-30 18:44:39 +0800

    update event controller

--- api/event/controllers/event.js
+++ api/event/controllers/event.js
@@ -10,9 +10,9 @@ module.exports = {
   async find(ctx) {
     let entities;
     if (ctx.query._q) {
-      entities = await strapi.services.event.search(ctx.query, ['image', 'users_permissions_user', 'event_participants']);
+      entities = await strapi.services.event.search(ctx.query, ['image', 'users_permissions_user', 'event_participants', 'event_participants.users_permissions_user', 'event_participants.users_permissions_user.profile', 'event_participants.users_permissions_user.profile.result_test_image']);
     } else {
-      entities = await strapi.services.event.find(ctx.query, ['image', 'users_permissions_user', 'event_participants']);
+      entities = await strapi.services.event.find(ctx.query, ['image', 'users_permissions_user', 'event_participants', 'event_participants.users_permissions_user', 'event_participants.users_permissions_user.profile', 'event_participants.users_permissions_user.profile.result_test_image']);
     }
 
     return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.event }));
